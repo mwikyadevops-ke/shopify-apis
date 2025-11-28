c2407e26103824f5cfb864203eec1bde2de82f8d
mwikyadevops-ke <mwikyadevops@gmail.com>
2025-11-28 12:55:43 +0300

    Refactor allowed origins in CORS configuration to use regex for Vercel domains

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -2,8 +2,7 @@ const allowedOrigins = [
     "http://localhost:5173",
     "https://optusconnect.co.ke",
     "https://www.optusconnect.co.ke",
-    "https://shopify-ui-teal.vercel.app",
-    "https://www.shopify-ui-teal.vercel.app"
+    /\.vercel\.app$/
 ];
 
 
