4500f5a892a17f7224a89c55b363a212afc72f1b
mwikyadevops-ke <mwikyadevops@gmail.com>
2025-11-28 12:58:32 +0300

    Update allowed origins in CORS configuration to use wildcard for Vercel domains

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     "http://localhost:5173",
     "https://optusconnect.co.ke",
     "https://www.optusconnect.co.ke",
-    /\.vercel\.app$/
+    "https://*.vercel.app"
 ];
 
 
