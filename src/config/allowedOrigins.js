cc519230d71b4badb45e62f78e067c80cecb955d
mwikyadevops-ke <mwikyadevops@gmail.com>
2025-11-28 12:33:10 +0300

    Add new allowed origins for CORS configuration to support additional domains

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,7 +1,10 @@
 const allowedOrigins = [
     "http://localhost:5173",
     "https://optusconnect.co.ke",
-    "https://www.optusconnect.co.ke"];
+    "https://www.optusconnect.co.ke",
+"https://shopify-ui-teal.vercel.app",
+"https://www.shopify-ui-teal.vercel.app"
+];
 
 
 export default allowedOrigins;
